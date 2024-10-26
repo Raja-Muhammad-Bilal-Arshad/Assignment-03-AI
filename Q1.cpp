cdf1faad2b0d0e709be0e1eec0a6c2d544f8a50c
Raja Muhammad Bilal Arshad <bilall.dude@gmail.com>
2024-10-27 00:21:45 +0500

    summary

--- Q1.cpp
+++ Q1.cpp
@@ -14,14 +14,11 @@ int main()
             D4=a%10; a=a/10;
             if((n>=100 && n<=100000) && (( ((D1==1 || D1==0)&&(D2==1 || D2==0)&&(D3==1 || D3==0)&&(D4==1 || D4==0)&& (D5==1 || D5==0)) &&(S==1)) || ( ((D1>=0 && D1<8)&&(D2>=0 || D2<0)&&(D3>=0 && D3<8)&&(D4>=0 && D4<8)&& (D5>=0 && D5<8)) &&(S==2))))
     switch(S)
-    {    case 1:
-        result=D1+D2*(2)+D3*(2*2)+D4*(2*2*2)+D5*(2*2*2*2); break;       
-        case 2:
-        result=D1+D2*(8)+D3*(8*8)+D4*(8*8*8)+D5*(8*8*8*8); break;
+    {    case 1: result=D1+D2*(2)+D3*(2*2)+D4*(2*2*2)+D5*(2*2*2*2); break;       
+         case 2: result=D1+D2*(8)+D3*(8*8)+D4*(8*8*8)+D5*(8*8*8*8); break;
     }
     else 
     {cout<<"INVALID .............ERROR.............."; return 1;}
    cout<<"The result is: "<< result<<endl;
    system("pause");
-   return 0;
-}
+   return 0;}
